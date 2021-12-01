7b6bf8e1bf5694f1ab563b5d344215356b92a8f3
Sören Kuklau <chucker23n+github@gmail.com>
2021-12-01 23:27:42 +0100

    day 1 part 1 also solved in C#

---
+++ day-1/adventofcode-2021-day-1-csharp/adventofcode-2021-day-1/Program.cs
@@ -0,0 +1,19 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+int count = 0;
+int? previousMeasurement = null;
+
+foreach (var item in lines)
+{
+    var currentMeasurement = int.Parse(item);
+
+    if (previousMeasurement == null)
+        previousMeasurement = currentMeasurement;
+
+    if (currentMeasurement > previousMeasurement)
+        count++;
+
+    previousMeasurement = currentMeasurement;
+}
+
+Console.WriteLine(count);
