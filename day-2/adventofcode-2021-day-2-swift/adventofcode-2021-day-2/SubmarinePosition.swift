218495f334d4128a35d207497d007160285b27f0
Sören Kuklau <chucker23n+github@gmail.com>
2021-12-03 22:56:39 +0100

    day 2 part 1 solved in Swift

---
+++ day-2/adventofcode-2021-day-2-swift/adventofcode-2021-day-2/SubmarinePosition.swift
@@ -0,0 +1,13 @@
+//
+//  SubmarinePosition.swift
+//  adventofcode-2021-day-2
+//
+//  Created by Sören Kuklau on 03.12.21.
+//
+
+import Foundation
+
+struct SubmarinePosition {
+    var Horizontal: Int
+    var Depth: Int
+}
---
+++ day-2/adventofcode-2021-day-2-swift/adventofcode-2021-day-2/main.swift
@@ -0,0 +1,47 @@
+//
+//  main.swift
+//  adventofcode-2021-day-2
+//
+//  Created by Sören Kuklau on 02.12.21.
+//
+
+import Foundation
+
+calculatePosition(filename: "test-input.txt")
+
+calculatePosition(filename: "input.txt")
+
+func calculatePosition(filename : String) {
+    var position = SubmarinePosition(Horizontal: 0, Depth: 0)
+    
+    let content = try? String(contentsOfFile: filename)
+    
+    let regex = try! NSRegularExpression(pattern: #"(?<Command>\w+) (?<Argument>\d+)"#)
+    
+    for line in content!.components(separatedBy: .newlines) {
+        if line == "" {
+            break
+        }
+        
+        let range = NSRange(location: 0, length: line.utf16.count)
+        
+        let match = regex.firstMatch(in: line, options: [], range: range)!
+        
+        let command = line[Range(match.range(withName: "Command"), in: line)!]
+        let argument = Int(line[Range(match.range(withName: "Argument"), in: line)!])!
+        
+        switch command {
+            case "forward":
+                position.Horizontal += argument
+            case "up":
+                position.Depth -= argument
+            case "down":
+                position.Depth += argument
+            default:
+                continue
+        }
+    }
+    
+    print (position)
+    print (position.Horizontal * position.Depth)
+}
